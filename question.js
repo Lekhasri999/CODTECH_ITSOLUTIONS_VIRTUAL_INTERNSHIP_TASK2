76e4789233c5ca3518c1cbc47ea79c566391ac4f
Lekhasri Narayanadasu <148227034+Lekhasri999@users.noreply.github.com>
2024-01-29 14:52:13 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,48 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following sorting algorithms has an average-case time complexity of O(n log n)?",
+    answer: "Quick Sort",
+    options: [
+      "Bubble Sort",
+      "Insertion Sort",
+      "Quick Sort",
+      "Selection Sort"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is the purpose of the __init__ method in a Python class?",
+    answer: "It is called when an object is created and initializes its attributes",
+    options: [
+      "It is called when an object is created and initializes its attributes",
+      "It is used to define a class method",
+      "It is called when an object is deleted",
+      " It is a built-in Python function and not specific to class initialization"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Of the following sort algorithms, which has execution time that is least dependent on initial ordering of the input ?",
+    answer: "Merge sort",
+    options: [
+      "Selection sort",
+      "Insertion sort",
+      "Quick sort",
+      "Merge sort"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which data structure in Python is typically used to implement a Last In, First Out (LIFO) behavior?",
+    answer:"Stack",
+    options: [
+      "Queue",
+      "Stack",
+      "Set",
+      "List"
+    ]
+  },
+    
+];
+
